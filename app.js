4d24fc447b08fee80409b7a0a8710e33c85ad866
Chris Lankford <christopherlankford@gmail.com>
2016-05-21 10:15:25 -0700

    Setting up a lightweight scaffolding for this service

---
+++ app.js
@@ -0,0 +1,60 @@
+var express = require('express');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+
+var projects = require('./routes/projects');
+var project = require('./routes/project');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/projects', projects);
+app.use('/project', project);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.send({
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.send({
+    message: err.message,
+    error: {}
+  });
+});
+
+app.use(function(err, req, res, next) {
+    if (req.url === '/favicon.ico') {
+        res.writeHead(200, {'Content-Type': 'image/x-icon'} );
+        res.end();
+        console.log('favicon requested');
+    }
+    res.send({
+        message: err.message,
+        error: {}
+    });
+});
+
+module.exports = app;
