14fa42df3b9679f2b1a4658e51fd9003372ec17f
Chris Lankford <christopherlankford@gmail.com>
2016-05-22 17:52:16 -0700

    Added support for getting project by id

--- routes/project.js
+++ routes/project.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
-/* GET project listing. */
-router.get('/', function(req, res) {
-    res.send('respond with a project json');
+/* GET project by ID. */
+router.get('/:id', function(req, res) {
+     fs.readFile( './data/projects.json', 'utf8', function (err, data) {
+       var projects = JSON.parse(data);
+       var projectId = null;
+       for (i = 0; i < projects.length; i++) {
+           if (req.params.id == projects[i]._id) {
+               console.log(projects[i]._id);
+               projectId = projects[i]._id;
+           }
+       }
+       if (projectId) {
+           res.send(projectId);
+       }
+       else {
+           res.status(404).send('Project not found');
+       }
+    });
 });
 
 module.exports = router;
